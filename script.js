518bc801c291bbfb4dde0794205095284c02651f
Anjali2104 <anjaliu949@gmail.com>
2023-07-18 00:47:17 +0530

    Contact-section added

--- script.js
+++ script.js
@@ -43,3 +43,19 @@ window.addEventListener("scroll", ()=>{
     phones[1].style.transform = `translateX(calc(700vh - ${offsetY}px))`
 
 })
+// FORM
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxlKH3RAYXfOsGIPn_GwiIEsChx9gCeIbbKJUNXj6pItv5FL6kCBqgYL6vXn6g_tM5y/exec'
+const form = document.forms['submit-to-google-sheet']
+const  msg = document.getElementById('msg')
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response =>{
+       msg.innerHTML = "Message sent successfully"
+       setTimeout(function(){
+          msg.innerHTML=''
+       },5000)
+       form.reset();
+    })
+    .catch(error => console.error('Error!', error.message))
+})
